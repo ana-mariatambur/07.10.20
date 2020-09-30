9e15deaf181b0467d9748c35a8eb55aecd8bca2d
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2020-09-30 15:30:30 +0300

    Delete problema8.py

--- problema8.py
+++
@@ -1,6 +0,0 @@
-"""Se introduc de la tastatură trei cifre. Afişaţi pe aceeaşi linie 5 numere formate cu
-aceste cifre luate o singură dată."""
-a=int(input("introduceti 1 numar:"))
-b=int(input("introduceti 2 numar:"))
-c=int(input("introduceti 3 numar:"))
-print(str(a)+str(b)+str(c),str(a)+str(c)+str(b),str(b)+str(a)+str(c),str(b)+str(c)+str(a),str(c)+str(a)+str(b))
