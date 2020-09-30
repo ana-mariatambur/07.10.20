de2d79f13b50904dfcc5930b2dc4a49d3f1ab6f9
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2020-09-30 15:22:13 +0300

    Delete problema2.py

--- problema2.py
+++
@@ -1,6 +0,0 @@
-"""Într-o tabără numărul de băieţi este cu 10 mai mare decât cel al fetelor. Dacă se citeşte
-de la tastatură numărul de fete, să se spună câţi elevi sunt în tabără."""
-fete=int(input("introduceti numarul de fete:="))
-baieti=fete+10
-total=fete+baieti
-print("in tabara sunt", total,"copii")
