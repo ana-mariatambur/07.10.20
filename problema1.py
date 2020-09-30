332ff3a356049bf40544befc11cc1d43453f27a1
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2020-09-30 10:29:33 +0300

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -2,17 +2,17 @@
 denumirea culorii. Convenim că culoarea roşie are numărul de rînd 1."""
 n=int(input("introduceti numarul culorii="))
 print("ati introdus culoarea cu numarul:,",n)
-if n=1:
+if n==1:
 print("rosu")
-if n=2:
+if n==2:
 print("oranj")
-if n=3:
+if n==3:
 print("galben")
-if n=4:
+if n==4:
 print("verde")
-if n=5:
+if n==5:
 print("albastru")
-if n=6:
+if n==6:
 print("indigo")
-if n=7:
+if n==7:
 print("violet")
