692b241511ca24f41ed4c1fdf16efcfaa81d40c5
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2020-09-30 15:30:40 +0300

    Delete problema9.py

--- problema9.py
+++
@@ -1,8 +0,0 @@
-"""Date trei numere, să se calculeze toate sumele posibile de câte două numere. Afişarea
-să cuprindă şi termenii sumei, nu numai valoarea ei."""
-a=2
-b=8
-c=6
-print(a,"+",b,"=",a+b)
-print(a,"+",c,"=",a+c)
-print(b,"+",c,"=",b+c)
